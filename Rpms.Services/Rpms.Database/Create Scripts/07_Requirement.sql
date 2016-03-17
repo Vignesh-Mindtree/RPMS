fd897b369ac08b91a84d63c20cb8d85c565c8fdd
Yashaswini R <yashaswini90@gmail.cim>
2016-03-17 16:02:30 +0530

    Review comment fixes

--- Rpms.Services/Rpms.Database/Create Scripts/07_Requirement.sql
+++ Rpms.Services/Rpms.Database/Create Scripts/07_Requirement.sql
@@ -6,5 +6,7 @@
     [Description] NVARCHAR(MAX) NOT NULL, 
     [Min_Year_Experience] INT NOT NULL DEFAULT 0, 
     [Max_Year_Experience] INT NOT NULL DEFAULT 0, 
-    [Location_Id] INT NOT NULL
+    [Location_Id] INT NOT NULL, 
+    CONSTRAINT [FK_Requirement_Project] FOREIGN KEY ([Project_Id]) REFERENCES [Project]([Id]), 
+    CONSTRAINT [FK_Requirement_City] FOREIGN KEY ([Location_Id]) REFERENCES City([Id])
 )
--- Rpms.Services/Rpms.Database/Create Scripts/10_EmployeeRequirementMapping.sql
+++ Rpms.Services/Rpms.Database/Create Scripts/10_EmployeeRequirementMapping.sql
@@ -2,5 +2,7 @@
 (
 	[Employee_Id] INT NOT NULL, 
     [Requirement_Id] INT NOT NULL, 
-    PRIMARY KEY ([Requirement_Id], [Employee_Id])
+    PRIMARY KEY ([Requirement_Id], [Employee_Id]), 
+    CONSTRAINT [FK_EmployeeRequirementMapping_Employee] FOREIGN KEY ([Employee_Id]) REFERENCES [Employee]([Id]), 
+    CONSTRAINT [FK_EmployeeRequirementMapping_Requirement] FOREIGN KEY ([Requirement_Id]) REFERENCES [Requirement]([Id])
 )
